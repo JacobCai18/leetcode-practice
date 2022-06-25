c4a57848ad086e76cc489eeb00a61d496aa6ca4a
Jacob Cai <cabbagecai18@gmail.com>
2022-06-26 05:26:49 +0800

    Lc24: Init

---
+++ src/main/java/com/jacobcai/Lc24.java
@@ -0,0 +1,31 @@
+package com.jacobcai;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lc24 {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+    }
+
+    public static void main(String[] args) {
+        ListNode n4 = new ListNode(4);
+        ListNode n3 = new ListNode(3, n4);
+        ListNode n2 = new ListNode(2, n3);
+        ListNode n1 = new ListNode(1, n2);
+        Lc24 lc24 = new Lc24();
+        ListNode head = lc24.swapPairs(n1);
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        System.out.println("list = " + list);
+    }
+}
