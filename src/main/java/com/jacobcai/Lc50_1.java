ee62f902c858dd09d4d039b11d28d1adbb18adf4
Jacob Cai <cabbagecai18@gmail.com>
2022-07-07 22:27:36 +0800

    Lc50: Init

---
+++ src/main/java/com/jacobcai/Lc50_1.java
@@ -0,0 +1,26 @@
+package com.jacobcai;
+
+public class Lc50_1 {
+    // 快速幂+递归
+    public double myPow(double x, int n) {
+        // 如果用int，n取Integer.MIN_VALUE时-n会越界
+        long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    }
+
+    private double quickMul(double x, long N) {
+        if (N == 0) {
+            return 1.0;
+        }
+        double y = quickMul(x, N / 2);
+        return N % 2 == 0 ? y * y : y * y * x;
+    }
+
+    public static void main(String[] args) {
+        double x = 2.00000;
+        int n = Integer.MIN_VALUE;
+        Lc50_1 lc50_1 = new Lc50_1();
+        double res = lc50_1.myPow(x, n);
+        System.out.println("res = " + res);
+    }
+}
