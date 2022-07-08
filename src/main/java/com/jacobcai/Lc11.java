b89fc57baa1878b9a8891633ba5754c6d5e13ef3
Jacob Cai <cabbagecai18@gmail.com>
2022-07-08 15:46:39 +0800

    Lc11: Init

---
+++ src/main/java/com/jacobcai/Lc11.java
@@ -0,0 +1,31 @@
+package com.jacobcai;
+
+public class Lc11 {
+    public int maxArea(int[] height) {
+        // 最大水量
+        int res = 0;
+        // 当前水量
+        int curr;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                curr = height[left] * (right - left);
+                res = Math.max(res, curr);
+                left++;
+            } else {
+                curr = height[right] * (right - left);
+                res = Math.max(res, curr);
+                right--;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        Lc11 lc11 = new Lc11();
+        int res = lc11.maxArea(height);
+        System.out.println("res = " + res);
+    }
+}
