345491ef2fa3936bcf9183d13330fb8b827be671
Jacob Cai <cabbagecai18@gmail.com>
2022-07-09 08:17:27 +0800

    Lc26: Init

---
+++ src/main/java/com/jacobcai/Lc26.java
@@ -0,0 +1,23 @@
+package com.jacobcai;
+
+public class Lc26 {
+    public int removeDuplicates(int[] nums) {
+        int slow = 0;
+        int fast = 1;
+        while (fast < nums.length) {
+            if (nums[slow] != nums[fast]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+            fast++;
+        }
+        return slow + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2};
+        Lc26 lc26 = new Lc26();
+        int res = lc26.removeDuplicates(nums);
+        System.out.println("res = " + res);
+    }
+}
