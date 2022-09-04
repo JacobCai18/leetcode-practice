32715c785e65e7655210b594d9d4347dd8b3569c
Jacob Cai <cabbagecai18@gmail.com>
2022-09-05 01:57:08 +0800

    Lc102: Init

---
+++ src/main/java/com/jacobcai/ci/Lc102.java
@@ -0,0 +1,51 @@
+package com.jacobcai.ci;
+
+import com.jacobcai.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class Lc102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            List<Integer> cur = new ArrayList<>();
+            for (int i = 0; i < len; i++) {
+                TreeNode node = queue.poll();
+                cur.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(cur);
+        }
+        return res;
+    }
+    
+    public static void main(String[] args) {
+        TreeNode n3, n9, n20, n15, n7;
+        n15 = new TreeNode(15);
+        n7 = new TreeNode(7);
+        n20 = new TreeNode(20,
+                           n15,
+                           n7);
+        n9 = new TreeNode(9);
+        n3 = new TreeNode(3,
+                          n9,
+                          n20);
+        Lc102 lc102 = new Lc102();
+        List<List<Integer>> res = lc102.levelOrder(n3);
+        System.out.println("res = " + res);
+    }
+}
