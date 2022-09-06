67152fddf3315fa1bc7b82bda8c40b221ec5ef85
Jacob Cai <cabbagecai18@gmail.com>
2022-09-06 10:15:17 +0800

    Lc120: 滚动数组

--- src/main/java/com/jacobcai/ci/Lc120.java
+++ src/main/java/com/jacobcai/ci/Lc120.java
@@ -6,19 +6,19 @@ import java.util.List;
 public class Lc120 {
     public int minimumTotal(List<List<Integer>> triangle) {
         int rowLen = triangle.size();
-        // dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
-        int[][] dp = new int[rowLen][rowLen];
-        dp[0][0] = triangle.get(0).get(0);
+        // dp[j] = Math.min(dp[j-1], dp[j]) + triangle[i][j]
+        int[] dp = new int[rowLen];
+        dp[0] = triangle.get(0).get(0);
         for (int i = 1; i < rowLen; i++) {
-            dp[i][0] = dp[i - 1][0] + triangle.get(i).get(0);
-            for (int j = 1; j < i; j++) {
-                dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle.get(i).get(j);
+            dp[i] = dp[i - 1] + triangle.get(i).get(i);
+            for (int j = i - 1; j >= 1; j--) {
+                dp[j] = Math.min(dp[j - 1], dp[j]) + triangle.get(i).get(j);
             }
-            dp[i][i] = dp[i - 1][i - 1] + triangle.get(i).get(i);
+            dp[0] = dp[0] + triangle.get(i).get(0);
         }
-        int res = dp[rowLen - 1][0];
+        int res = dp[0];
         for (int j = 1; j < rowLen; j++) {
-            res = Math.min(res, dp[rowLen - 1][j]);
+            res = Math.min(res, dp[j]);
         }
         return res;
     }
