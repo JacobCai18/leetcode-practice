c5a8d4b18f0f69ce1b79db4c3b70e9de7a62e105
Jacob Cai <cabbagecai18@gmail.com>
2022-09-06 14:02:40 +0800

    Lc120_2: 滚动数组

--- src/main/java/com/jacobcai/ci/Lc120_2.java
+++ src/main/java/com/jacobcai/ci/Lc120_2.java
@@ -6,16 +6,16 @@ import java.util.List;
 public class Lc120_2 {
     public int minimumTotal(List<List<Integer>> triangle) {
         int rowLen = triangle.size();
-        // dp[i][j]: 底部到当前位置的最小路径和
-        // dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j]
+        // dp[j]: 底部到当前位置的最小路径和
+        // dp[j] = Math.min(dp[j], dp[j + 1]) + triangle[i][j]
         // 将dp[rowLen + 1]这一行都置为0
-        int[][] dp = new int[rowLen + 1][rowLen + 1];
+        int[] dp = new int[rowLen + 1];
         for (int i = rowLen - 1; i >= 0; i--) {
             for (int j = 0; j <= i; j++) {
-                dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle.get(i).get(j);
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
     
     public static void main(String[] args) {
