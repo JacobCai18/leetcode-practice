d4ba50faf41a6497a0cb0332cd653c3f877d33a3
Jacob Cai <cabbagecai18@gmail.com>
2022-09-06 13:58:23 +0800

    Lc120_2: 自底向上

---
+++ src/main/java/com/jacobcai/ci/Lc120_2.java
@@ -0,0 +1,30 @@
+package com.jacobcai.ci;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lc120_2 {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int rowLen = triangle.size();
+        // dp[i][j]: 底部到当前位置的最小路径和
+        // dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j]
+        // 将dp[rowLen + 1]这一行都置为0
+        int[][] dp = new int[rowLen + 1][rowLen + 1];
+        for (int i = rowLen - 1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0][0];
+    }
+    
+    public static void main(String[] args) {
+        List<List<Integer>> triangle = new ArrayList<>(List.of(List.of(2),
+                                                               List.of(3, 4),
+                                                               List.of(6, 5, 7),
+                                                               List.of(4, 1, 8, 3)));
+        Lc120_2 lc120_2 = new Lc120_2();
+        int res = lc120_2.minimumTotal(triangle);
+        System.out.println("res = " + res);
+    }
+}
