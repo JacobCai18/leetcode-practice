a011c086695c47ed89504a63c0a718ea0ace69f4
Jacob Cai <cabbagecai18@gmail.com>
2022-09-07 01:21:29 +0800

    Lc141: Update v1.0

--- src/main/java/com/jacobcai/ci/Lc141.java
+++ src/main/java/com/jacobcai/ci/Lc141.java
@@ -4,20 +4,19 @@ import com.jacobcai.ListNode;
 
 public class Lc141 {
     public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) {
+        if (head == null) {
             return false;
         }
         // 快慢指针
         ListNode slow, fast;
-        slow = head;
-        fast = head.next;
+        slow = fast = head;
         while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
             // 快慢指针相遇时有环
             if (slow == fast) {
                 return true;
             }
-            slow = slow.next;
-            fast = fast.next.next;
         }
         return false;
     }
