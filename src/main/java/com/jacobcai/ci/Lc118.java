789013f59447408ad54f2a46a679eedcd4760e19
Jacob Cai <cabbagecai18@gmail.com>
2022-09-06 09:45:03 +0800

    Lc118: Update v1.0

--- src/main/java/com/jacobcai/ci/Lc118.java
+++ src/main/java/com/jacobcai/ci/Lc118.java
@@ -5,16 +5,14 @@ import java.util.List;
 
 public class Lc118 {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < numRows; i++) {
+        List<List<Integer>> res = new ArrayList<>(List.of(List.of(1)));
+        for (int i = 1; i < numRows; i++) {
             List<Integer> cur = new ArrayList<>();
-            for (int j = 0; j <= i; j++) {
-                if (j == 0 || j == i) {
-                    cur.add(1);
-                } else {
-                    cur.add(res.get(i - 1).get(j - 1) + res.get(i - 1).get(j));
-                }
+            cur.add(1);
+            for (int j = 1; j < i; j++) {
+                cur.add(res.get(i - 1).get(j - 1) + res.get(i - 1).get(j));
             }
+            cur.add(1);
             res.add(cur);
         }
         return res;
