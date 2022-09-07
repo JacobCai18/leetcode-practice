fe800a5514a20fc4227b5254c41c906f4082979e
Jacob Cai <cabbagecai18@gmail.com>
2022-09-07 13:36:40 +0800

    Lc209: minSubArrayLen

---
+++ src/main/java/com/jacobcai/cii/Lc209.java
@@ -0,0 +1,32 @@
+package com.jacobcai.cii;
+
+public class Lc209 {
+    public int minSubArrayLen(int target,
+                              int[] nums) {
+        int sum, le, ri, minLen;
+        sum = 0;
+        // window: [le, ri)
+        le = ri = 0;
+        minLen = Integer.MAX_VALUE;
+        while (ri < nums.length) {
+            sum += nums[ri++];
+            while (sum >= target) {
+                minLen = Math.min(minLen,
+                                  ri - le);
+                sum -= nums[le++];
+            }
+        }
+        return minLen == Integer.MAX_VALUE ?
+               0 :
+               minLen;
+    }
+    
+    public static void main(String[] args) {
+        int target = 7;
+        int[] nums = {2, 3, 1, 2, 4, 3};
+        Lc209 lc209 = new Lc209();
+        int res = lc209.minSubArrayLen(target,
+                                       nums);
+        System.out.println("res = " + res);
+    }
+}
