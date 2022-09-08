ce6b4625e8c6802d2eda5a521b4ede8db27ed3ba
Jacob Cai <cabbagecai18@gmail.com>
2022-09-09 02:29:08 +0800

    Lc416_3: boolean, dp[i][j]

---
+++ src/main/java/com/jacobcai/d/Lc416_3.java
@@ -0,0 +1,35 @@
+package com.jacobcai.d;
+
+public class Lc416_3 {
+    public boolean canPartition(int[] nums) {
+        // tar: 「等和子集」的和
+        int len, sum, tar;
+        sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        // sum为奇数时nums不能被分为两个「等和子集」
+        if ((sum & 1) == 1) {
+            return false;
+        }
+        // dp[i][j]: 前i个数字，能选出数字组合使数字总和恰好为j
+        // dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i]]
+        boolean[][] dp = new boolean[(len = nums.length) + 1][(tar = sum / 2) + 1];
+        // 前0个数字能选出数字组合使数字总和恰好为0
+        dp[0][0] = true;
+        for (int i = 1; i <= len; i++) {
+            int num = nums[i - 1];
+            for (int j = 0; j <= tar; j++) {
+                dp[i][j] = dp[i - 1][j] || (j - num >= 0 && dp[i - 1][j - num]);
+            }
+        }
+        return dp[len][tar];
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {1, 5, 11, 5};
+        Lc416_3 lc416_3 = new Lc416_3();
+        boolean res = lc416_3.canPartition(nums);
+        System.out.println("res = " + res);
+    }
+}
