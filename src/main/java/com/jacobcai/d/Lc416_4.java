c6714740310ba3507ddb2772ba6b10edc069bf72
Jacob Cai <cabbagecai18@gmail.com>
2022-09-09 12:32:57 +0800

    Lc416_4: 剪枝v3.0

--- src/main/java/com/jacobcai/d/Lc416_4.java
+++ src/main/java/com/jacobcai/d/Lc416_4.java
@@ -3,19 +3,22 @@ package com.jacobcai.d;
 public class Lc416_4 {
     public boolean canPartition(int[] nums) {
         // tar: 「等和子集」的和
-        int sum, tar;
-        sum = 0;
+        int sum, max, tar;
+        sum = max = 0;
         for (int num : nums) {
             sum += num;
+            if (num > max) {
+                max = num;
+            }
         }
         // sum为奇数时nums不能被分为两个「等和子集」
-        if ((sum & 1) == 1) {
+        if ((sum & 1) == 1 || max > (tar = sum / 2)) {
             return false;
         }
         // dp[i][j]: 前i个数字，能选出数字组合使数字总和恰好为j
         // dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i]]
         // dp[j] = dp[j] || dp[j-nums[i]]
-        boolean[] dp = new boolean[(tar = sum / 2) + 1];
+        boolean[] dp = new boolean[tar + 1];
         // 前0个数字能选出数字组合使数字总和恰好为0
         dp[0] = true;
         for (int num : nums) {
