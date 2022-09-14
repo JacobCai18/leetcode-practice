3ef60013719ae8b7f361df110c618b1f4ecc2d8e
Jacob Cai <cabbagecai18@gmail.com>
2022-09-14 22:12:30 +0800

    Lc122_2: 贪心

---
+++ src/main/java/com/jacobcai/cc/Lc122_2.java
@@ -0,0 +1,22 @@
+package com.jacobcai.cc;
+
+public class Lc122_2 {
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        int res = 0;
+        for (int i = 1; i < len; i++) {
+            // 今天价格比昨天高，昨天就买并今天卖出
+            // 今天价格比昨天低，昨天就不买
+            res += Math.max(prices[i] - prices[i - 1],
+                            0);
+        }
+        return res;
+    }
+    
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        Lc122_2 lc122_2 = new Lc122_2();
+        int res = lc122_2.maxProfit(prices);
+        System.out.println("res = " + res);
+    }
+}
