d316772b94ae2b34c65e96b9ea0d07c3e3648ade
Jacob Cai <cabbagecai18@gmail.com>
2022-09-14 22:04:51 +0800

    Lc122: DP, 一维

--- src/main/java/com/jacobcai/cc/Lc122.java
+++ src/main/java/com/jacobcai/cc/Lc122.java
@@ -6,18 +6,17 @@ public class Lc122 {
         // dp[i][j]: 第i天状态j时的最大利润
         // j=0：持有现金
         // j=1：持有股票
-        int[][] dp = new int[len][2];
+        int[] dp = new int[2];
         // 第0天持有现金的最大利润（本金为0）
-        dp[0][0] = 0;
         // 第0天持有股票的最大利润
-        dp[0][1] = -prices[0];
+        dp[1] = -prices[0];
         for (int i = 1; i < len; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0],
-                                dp[i - 1][1] + prices[i]);
-            dp[i][1] = Math.max(dp[i - 1][1],
-                                dp[i - 1][0] - prices[i]);
+            dp[0] = Math.max(dp[0],
+                             dp[1] + prices[i]);
+            dp[1] = Math.max(dp[1],
+                             dp[0] - prices[i]);
         }
-        return dp[len - 1][0];
+        return dp[0];
     }
     
     public static void main(String[] args) {
