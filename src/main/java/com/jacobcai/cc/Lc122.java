5a6deb6eb8c85fd834be789cbd634f8be84fe196
Jacob Cai <cabbagecai18@gmail.com>
2022-09-15 02:49:22 +0800

    Lc122: 证明空间优化后dp[0]含义改变但结果仍正确

--- src/main/java/com/jacobcai/cc/Lc122.java
+++ src/main/java/com/jacobcai/cc/Lc122.java
@@ -13,6 +13,9 @@ public class Lc122 {
         for (int i = 1; i < len; i++) {
             dp[0] = Math.max(dp[0],
                              dp[1] + prices[i]);
+            // 若昨天价格高于今天，则dp[0]仍是昨天的，符合未空间优化时的dp[i-1][0]
+            // 若昨天价格低于今天，则dp[0]虽是今天的，但dp[i][0]-prices[i]==dp[1]，不会改变未空间优化时的结果，
+            // 相当于昨天买，今天卖了再买，和昨天已持有股票是一样的
             dp[1] = Math.max(dp[1],
                              dp[0] - prices[i]);
         }
