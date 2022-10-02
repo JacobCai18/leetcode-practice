f13526d5617100a0960f6f3b95da15ea815a11c4
Jacob Cai <cabbagecai18@gmail.com>
2022-10-02 18:58:33 +0800

    ConsecutiveNums

---
+++ src/main/java/com/jacobcai/cc/Lc180.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS Logs;
+Create table Logs
+(
+    id  int,
+    num int
+);
+insert into Logs (id, num)
+values ('1', '1');
+insert into Logs (id, num)
+values ('2', '1');
+insert into Logs (id, num)
+values ('3', '1');
+insert into Logs (id, num)
+values ('4', '2');
+insert into Logs (id, num)
+values ('5', '1');
+insert into Logs (id, num)
+values ('6', '2');
+insert into Logs (id, num)
+values ('7', '2');
+
+select *
+from Logs;
+
+SELECT DISTINCT l1.Num AS ConsecutiveNums
+FROM Logs l1,
+     Logs l2,
+     Logs l3
+WHERE l1.Id = l2.Id - 1
+  AND l2.Id = l3.Id - 1
+  AND l1.Num = l2.Num
+  AND l2.Num = l3.Num
+;
