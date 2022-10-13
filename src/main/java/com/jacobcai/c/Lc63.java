18ca832508fe7b131e4660c63c26d444341fc65e
Jacob Cai <cabbagecai18@gmail.com>
2022-10-14 02:39:48 +0800

    v2.1: 优化初始行

--- src/main/java/com/jacobcai/c/Lc63.java
+++ src/main/java/com/jacobcai/c/Lc63.java
@@ -11,13 +11,7 @@ public class Lc63 {
         if (dp[0] == 0) {
             return 0;
         }
-        for (int j = 1; j < colLen; j++) {
-            if (obstacleGrid[0][j] == 1) {
-                break;
-            }
-            dp[j] = 1;
-        }
-        for (int i = 1; i < rowLen; i++) {
+        for (int i = 0; i < rowLen; i++) {
             if (obstacleGrid[i][0] == 1) {
                 dp[0] = 0;
             }
