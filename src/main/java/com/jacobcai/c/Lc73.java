dcca087599661fc12747f583c5a2acdf61562dcf
Jacob Cai <cabbagecai18@gmail.com>
2022-11-08 13:50:22 +0800

    Lc73-setZeros: matrix

---
+++ src/main/java/com/jacobcai/c/Lc73.java
@@ -0,0 +1,35 @@
+package com.jacobcai.c;
+
+import java.util.Arrays;
+
+public class Lc73 {
+    public void setZeros(int[][] matrix) {
+        int rowLen, colLen;
+        boolean[] row, col;
+        rowLen = matrix.length;
+        colLen = matrix[0].length;
+        row = new boolean[rowLen];
+        col = new boolean[colLen];
+        for (int i = 0; i < rowLen; i++) {
+            for (int j = 0; j < colLen; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = col[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < rowLen; i++) {
+            for (int j = 0; j < colLen; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        Lc73 lc73 = new Lc73();
+        lc73.setZeros(matrix);
+        System.out.println("matrix = " + Arrays.deepToString(matrix));
+    }
+}
