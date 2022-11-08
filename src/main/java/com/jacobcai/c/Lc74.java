76fd74675eb17162938ce46a3c7a1afbbc3db3b8
Jacob Cai <cabbagecai18@gmail.com>
2022-11-08 16:48:06 +0800

    Lc74-searchMatrix: 二分，矩阵映射

---
+++ src/main/java/com/jacobcai/c/Lc74.java
@@ -0,0 +1,34 @@
+package com.jacobcai.c;
+
+public class Lc74 {
+    public boolean searchMatrix(int[][] matrix,
+                                int target) {
+        int rowLen, colLen;
+        int le, ri;
+        rowLen = matrix.length;
+        colLen = matrix[0].length;
+        le = 0;
+        ri = rowLen * colLen - 1;
+        while (le <= ri) {
+            int mid = (le + ri) >>> 1;
+            int val = matrix[mid / colLen][mid % colLen];
+            if (target == val) {
+                return true;
+            } else if (target < val) {
+                ri = mid - 1;
+            } else {
+                le = mid + 1;
+            }
+        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+        int target = 3;
+        Lc74 lc74 = new Lc74();
+        boolean res = lc74.searchMatrix(matrix,
+                                        target);
+        System.out.println("res = " + res);
+    }
+}
