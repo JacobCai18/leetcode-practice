26e7dfeed6088783b6f9db83f03314480814397a
Jacob Cai <cabbagecai18@gmail.com>
2022-12-03 17:32:44 +0800

    Lc1769_2: Update v1.0

--- src/main/java/com/jacobcai/mdccc/Lc1769_2.java
+++ src/main/java/com/jacobcai/mdccc/Lc1769_2.java
@@ -6,33 +6,30 @@ public class Lc1769_2 {
     public int[] minOperations(String boxes) {
         // le: [0, i] 上 '1' 的个数
         // ri: (i, len - 1] 上 '1' 的个数
-        // count: 所有小球移动到 i 的最小操作数
-        int len, le, ri, count;
-        int[] res;
+        int len, le, ri;
+        // dp[i]: 所有小球移动到 i 的最小操作数
+        int[] dp;
         len = boxes.length();
         le = boxes.charAt(0) - '0';
         ri = 0;
-        count = 0;
-        // 计算 i = 0 位置上右边的 '1' 的个数，以及所有 '1' 移动到 i = 0 的操作数
+        dp = new int[len];
+        // 计算 i = 0 位置上右边的 '1' 的个数，以及所有 '1' 移动到 i = 0 的最小操作数
         for (int i = 1; i < len; i++) {
             if (boxes.charAt(i) == '1') {
                 ri++;
-                count += i;
+                dp[0] += i;
             }
         }
-        res = new int[len];
-        res[0] = count;
         for (int i = 1; i < len; i++) {
             // 根据上一个位置左边的 '1' 的个数和右边的 '1' 的个数计算所有小球移动到当前位置的操作数
-            count = count + le - ri;
-            res[i] = count;
+            dp[i] = dp[i - 1] + le - ri;
             // 更新当前位置左边的 '1' 的个数和右边的 '1' 的个数
             if (boxes.charAt(i) == '1') {
                 le++;
                 ri--;
             }
         }
-        return res;
+        return dp;
     }
     
     public static void main(String[] args) {
