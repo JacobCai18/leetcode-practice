700b8e7e629c32b7550d77659bdba36ea0456ac9
Jacob Cai <cabbagecai18@gmail.com>
2022-12-20 09:32:17 +0800

    Lc1760-minimumSize: 最大尺寸与最大操作数成反比，二分

---
+++ src/main/java/com/jacobcai/mdccc/Lc1760.java
@@ -0,0 +1,37 @@
+package com.jacobcai.mdccc;
+
+import java.util.Arrays;
+
+public class Lc1760 {
+    public int minimumSize(int[] nums,
+                           int maxOperations) {
+        int le = 1;
+        int ri = Arrays.stream(nums)
+                       .max()
+                       .getAsInt();
+        int res = 0;
+        while (le <= ri) {
+            int y = (le + ri) >>> 1;
+            long ops = 0;
+            for (int num : nums) {
+                ops += (num - 1) / y;
+            }
+            if (ops <= maxOperations) {
+                res = y;
+                ri = y - 1;
+            } else {
+                le = y + 1;
+            }
+        }
+        return res;
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {2, 4, 8, 2};
+        int maxOperations = 4;
+        Lc1760 lc1760 = new Lc1760();
+        int res = lc1760.minimumSize(nums,
+                                     maxOperations);
+        System.out.println("res = " + res);
+    }
+}
