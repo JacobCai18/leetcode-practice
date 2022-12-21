dff210cfec4d76ff36090ead705aa48e1d1c1b13
Jacob Cai <cabbagecai18@gmail.com>
2022-12-21 18:59:16 +0800

    Lc76: Update v1.0

--- src/main/java/com/jacobcai/c/Lc76.java
+++ src/main/java/com/jacobcai/c/Lc76.java
@@ -3,67 +3,46 @@ package com.jacobcai.c;
 public class Lc76 {
     public String minWindow(String s,
                             String t) {
-        // ASCII表总长128
-        // 目标 String 中各个 char 的出现次数
-        int[] need = new int[128];
-        for (int i = 0; i < t.length(); i++) {
-            need[t.charAt(i)]++;
-        }
-        // 当前 substring 中各个 char 的出现次数
-        int[] curr = new int[128];
-        // window: [left, right）
-        int left = 0;
-        int right = 0;
-        // minLength 初始值为一定不可达到的长度
-        int minLength = s.length() + 1;
+        int[] sWinFreq, tFreq;
+        int le, ri, start, minLen, sWinCount;
+        sWinFreq = new int[128];
+        tFreq = new int[128];
+        for (char c : t.toCharArray()) tFreq[c]++;
+        le = ri = start = 0;
+        minLen = s.length() + 1;
         // 当前 substring 中目标 String 出现的 char 的出现次数
-        int count = 0;
-        // minLength 的 substring 在 s 中的起始位置
-        int start = 0;
-        
-        while (right < s.length()) {
-            char charAtRight = s.charAt(right);
-            // right++ 至出现目标 char 的位置
-            if (need[charAtRight] == 0) {
-                right++;
+        sWinCount = 0;
+        while (ri < s.length()) {
+            char charAtRi = s.charAt(ri);
+            if (tFreq[charAtRi] == 0) {
+                ri++;
                 continue;
             }
             // 对应 char 未达到次数
-            if (curr[charAtRight] < need[charAtRight]) {
-                count++;
-            }
-            curr[charAtRight]++;
-            right++;
-            // 涵盖 t 所有字符：包含 t 中所有 char，对应的出现次数 >= t 中 char 出现次数
-            // 以 t.length() 作为判断
-            while (count == t.length()) {
-                // 不用 Math.min 是为了记录出现 minLength 时的 start
-                if (right - left < minLength) {
-                    minLength = right - left;
-                    start = left;
+            if (sWinFreq[charAtRi]++ < tFreq[charAtRi]) sWinCount++;
+            ri++;
+            // 窗口包含了所有 t 中字符
+            while (sWinCount == t.length()) {
+                // 不用 Math.min 是为了记录出现 minLen 时的 start
+                if (ri - le < minLen) {
+                    minLen = ri - le;
+                    start = le;
                 }
-                char charAtLeft = s.charAt(left);
-                // left++ 至出现目标 char 的位置
-                if (need[charAtLeft] == 0) {
-                    left++;
+                char charAtLe = s.charAt(le);
+                if (tFreq[charAtLe] == 0) {
+                    le++;
                     continue;
                 }
                 // 如果左边即将要去掉的字符被目标字符串需要，且出现的频次正好等于指定频次，那么如果去掉了这个字符，
-                // 就不满足覆盖子串的条件，此时要破坏循环条件跳出循环，即控制目标字符串指定字符的出现总频次 count - 1
-                if (curr[charAtLeft] == need[charAtLeft]) {
-                    count--;
-                }
-                curr[charAtLeft]--;
-                left++;
+                // 就不满足覆盖子串的条件，此时要破坏循环条件跳出循环，即控制目标字符串指定字符的出现总频次 sWinCount - 1
+                if (--sWinFreq[charAtLe] < tFreq[charAtLe]) sWinCount--;
+                le++;
             }
         }
-        // 如果 minLength 还为初始值，说明没有符合条件的 substring
-        if (minLength == s.length() + 1) {
-            return "";
-        }
+        // 如果 minLen 还为初始值，说明没有符合条件的 substring
+        if (minLen == s.length() + 1) return "";
         // 要先设计return
-        return s.substring(start,
-                           start + minLength);
+        return s.substring(start, start + minLen);
     }
     
     public static void main(String[] args) {
