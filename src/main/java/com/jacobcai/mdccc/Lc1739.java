39cd464e6a05d44c602992acfffcafe10b42ccde
Jacob Cai <cabbagecai18@gmail.com>
2022-12-25 19:07:50 +0800

    Lc1739-minimumBoxes: 贪心

---
+++ src/main/java/com/jacobcai/mdccc/Lc1739.java
@@ -0,0 +1,24 @@
+package com.jacobcai.mdccc;
+
+public class Lc1739 {
+    public int minimumBoxes(int n) {
+        int cur = 1, i = 1, j = 1;
+        while (cur < n) {
+            n -= cur;
+            cur += ++i;
+        }
+        cur = 1;
+        while (cur < n) {
+            n -= cur;
+            j++;
+            cur++;
+        }
+        return (i - 1) * i / 2 + j;
+    }
+    
+    public static void main(String[] args) {
+        Lc1739 lc1739 = new Lc1739();
+        int res = lc1739.minimumBoxes(15);
+        System.out.println("res = " + res);
+    }
+}
