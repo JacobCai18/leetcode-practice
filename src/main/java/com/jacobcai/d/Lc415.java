cb18e55c0bd56e01ccfac03e83bab631cd876a3a
Jacob Cai <cabbagecai18@gmail.com>
2023-01-01 18:16:00 +0800

    Lc415-addStrings

---
+++ src/main/java/com/jacobcai/d/Lc415.java
@@ -0,0 +1,34 @@
+package com.jacobcai.d;
+
+public class Lc415 {
+    public String addStrings(String num1,
+                             String num2) {
+        int i, j, c;
+        i = num1.length() - 1;
+        j = num2.length() - 1;
+        c = 0;
+        StringBuilder sb = new StringBuilder();
+        while (i >= 0 || j >= 0) {
+            int x = i >= 0 ?
+                    num1.charAt(i--) - '0' :
+                    0;
+            int y = j >= 0 ?
+                    num2.charAt(j--) - '0' :
+                    0;
+            int sum = x + y + c;
+            sb.append(sum % 10);
+            c = sum / 10;
+        }
+        return sb.reverse()
+                 .toString();
+    }
+    
+    public static void main(String[] args) {
+        String num1 = "11";
+        String num2 = "123";
+        Lc415 lc415 = new Lc415();
+        String res = lc415.addStrings(num1,
+                                      num2);
+        System.out.println("res = " + res);
+    }
+}
