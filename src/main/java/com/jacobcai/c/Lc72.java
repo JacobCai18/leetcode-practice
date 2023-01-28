1e11071fa5830f93f2ed22e598256c450bb794b7
Jacob Cai <cabbagecai18@gmail.com>
2023-01-28 19:49:04 +0800

    Lc72: 双串DP

--- src/main/java/com/jacobcai/c/Lc72.java
+++ src/main/java/com/jacobcai/c/Lc72.java
@@ -1,44 +1,26 @@
 package com.jacobcai.c;
 
 public class Lc72 {
-    public int minDistance(String word1,
-                           String word2) {
-        int len1 = word1.length();
-        int len2 = word2.length();
-        // 有一个字符串为空串
-        if (len1 * len2 == 0) {
-            return len1 + len2;
-        }
-        // DP 数组
-        int[][] dp = new int[len1 + 1][len2 + 1];
-        // 边界状态初始化
-        for (int i = 0; i < len1 + 1; i++) {
-            dp[i][0] = i;
-        }
-        for (int j = 0; j < len2 + 1; j++) {
-            dp[0][j] = j;
-        }
-        // 计算所有 DP 值
-        for (int i = 1; i < len1 + 1; i++) {
-            for (int j = 1; j < len2 + 1; j++) {
-                int left = dp[i - 1][j] + 1;
-                int down = dp[i][j - 1] + 1;
-                int leftDown = dp[i - 1][j - 1];
-                if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
-                    leftDown += 1;
-                }
-                dp[i][j] = Math.min(left,
-                                   Math.min(down,
-                                            leftDown));
+    public int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        if (m * n == 0) return m + n; // 有一个字符串为空串
+        int[][] dp = new int[m + 1][n + 1]; // dp[i][j] = 串 1 [1, i] 转换成串 2 [1, j] 的最少操作数
+        for (int i = 1; i < m + 1; i++) dp[i][0] = i;
+        for (int j = 1; j < n + 1; j++) dp[0][j] = j;
+        for (int i = 1; i < m + 1; i++)
+            for (int j = 1; j < n + 1; j++) {
+                int a, b, c;
+                a = dp[i - 1][j] + 1; // 串 1 插入
+                b = dp[i][j - 1] + 1; // 串 2 插入
+                c = dp[i - 1][j - 1] + (word1.charAt(i - 1) == word2.charAt(j - 1) ? 0 : 1); // 串 1 替换
+                dp[i][j] = Math.min(a, Math.min(b, c));
             }
-        }
-        return dp[len1][len2];
+        return dp[m][n];
     }
-    
+
     public static void main(String[] args) {
         Lc72 lc72 = new Lc72();
-        int res = lc72.minDistance("horse",
-                                   "ros");
+        int res = lc72.minDistance("horse", "ros");
         System.out.println("res = " + res);
     }
 }
