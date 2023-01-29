10613010597ad51801e6fb695ecaa8834caf7547
Jacob Cai <cabbagecai18@gmail.com>
2023-01-29 20:28:52 +0800

    Lc2541-minOperations

---
+++ src/main/java/com/jacobcai/mmdc/Lc2541.java
@@ -0,0 +1,26 @@
+package com.jacobcai.mmdc;
+
+import java.util.Arrays;
+
+public class Lc2541 {
+    public long minOperations(int[] nums1, int[] nums2, int k) {
+        if (k == 0) return Arrays.equals(nums1, nums2) ? 0 : -1;
+        int n = nums1.length, sum = 0;
+        long cnt = 0;
+        for (int i = 0; i < n; i++) {
+            int diff = nums1[i] - nums2[i];
+            if (diff % k != 0) return -1;
+            if (diff > 0) cnt += diff / k;
+            sum += diff;
+        }
+        return sum == 0 ? cnt : -1;
+    }
+
+    public static void main(String[] args) {
+        Lc2541 lc2541 = new Lc2541();
+        int[] nums1 = {4, 3, 1, 4};
+        int[] nums2 = {1, 3, 7, 1};
+        long res = lc2541.minOperations(nums1, nums2, 3);
+        System.out.println("res = " + res);
+    }
+}
