4dc609265361c16e57ae92e0ba45bc9eb100bacb
Jacob Cai <cabbagecai18@gmail.com>
2023-01-29 15:25:43 +0800

    Lc50-myPow: 快速幂

--- src/main/java/com/jacobcai/c/Lc50_1.java
+++ src/main/java/com/jacobcai/c/Lc50_1.java
@@ -2,34 +2,22 @@ package com.jacobcai.c;
 
 public class Lc50_1 {
     // 快速幂+递归
-    public double myPow(double x,
-                        int n) {
+    public double myPow(double x, int n) {
         // 如果用int，n取Integer.MIN_VALUE时-n会越界
-        return n >= 0 ?
-               quickMul(x,
-                        n) :
-               1.0 / quickMul(x,
-                              -(long) n);
+        return n >= 0 ? quickMul(x, n) : 1.0 / quickMul(x, -(long) n);
     }
     
-    private double quickMul(double x,
-                            long n) {
-        if (n == 0) {
-            return 1.0;
-        }
-        double y = quickMul(x,
-                            n / 2);
-        return n % 2 == 0 ?
-               y * y :
-               y * y * x;
+    private double quickMul(double x, long n) {
+        if (n == 0) return 1.0;
+        double y = quickMul(x, n / 2);
+        return (n & 1) == 1 ? y * y : y * y * x;
     }
     
     public static void main(String[] args) {
         double x = 2.00000;
         int n = 10;
         Lc50_1 lc50_1 = new Lc50_1();
-        double res = lc50_1.myPow(x,
-                                  n);
+        double res = lc50_1.myPow(x, n);
         System.out.println("res = " + res);
     }
 }
--- src/main/java/com/jacobcai/c/Lc50_2.java
+++ src/main/java/com/jacobcai/c/Lc50_2.java
@@ -2,37 +2,28 @@ package com.jacobcai.c;
 
 public class Lc50_2 {
     // 快速幂+迭代
-    public double myPow(double x,
-                        int n) {
-        return n >= 0 ?
-               quickMul(x,
-                        n) :
-               1.0 / quickMul(x,
-                              -(long) n);
+    public double myPow(double x, int n) {
+        return n >= 0 ? quickMul(x, n) : 1.0 / quickMul(x, -(long) n);
     }
-    
-    private double quickMul(double x,
-                            long n) {
+
+    private double quickMul(double x, long n) {
         double res = 1.0;
         double x_contribute = x;
         // 对N进行二进制拆分
         // e.g. 十进制10 == 二进制1010，x^10 == (x^2)^1 * (x^2)^3
         while (n > 0) {
-            if ((n & 1) == 1) {
-                res *= x_contribute;
-            }
+            if ((n & 1) == 1) res *= x_contribute;
             x_contribute *= x_contribute;
             n >>= 1;
         }
         return res;
     }
-    
+
     public static void main(String[] args) {
         double x = 2.00000;
         int n = 10;
         Lc50_2 lc50_2 = new Lc50_2();
-        double res = lc50_2.myPow(x,
-                                  n);
+        double res = lc50_2.myPow(x, n);
         System.out.println("res = " + res);
     }
 }
