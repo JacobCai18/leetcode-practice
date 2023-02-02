ef2e0d3d928047350152e829b234b9f696dd0ebc
Jacob Cai <cabbagecai18@gmail.com>
2023-02-02 20:55:52 +0800

    Lc139: 区间dp

--- src/main/java/com/jacobcai/cc/Lc139.java
+++ src/main/java/com/jacobcai/cc/Lc139.java
@@ -6,30 +6,27 @@ import java.util.List;
 import java.util.Set;
 
 public class Lc139 {
-    public boolean wordBreak(String s,
-                             List<String> wordDict) {
+    public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> wordDictSet = new HashSet<>(wordDict);
-        boolean[] dp = new boolean[s.length() + 1];
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
         dp[0] = true;
-        for (int i = 1; i <= s.length(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
-                    dp[i] = true;
+        for (int j = 1; j <= n; j++)
+            for (int i = j - 1; i > -1; i--)
+                if (dp[i] && wordDictSet.contains(s.substring(i, j))) {
+                    dp[j] = true;
                     break;
                 }
-            }
-        }
-        return dp[s.length()];
+        return dp[n];
     }
-    
+
     public static void main(String[] args) {
         String s = "leetcode";
         List<String> wordDict = new ArrayList<>();
         wordDict.add("leet");
         wordDict.add("code");
         Lc139 lc139 = new Lc139();
-        boolean res = lc139.wordBreak(s,
-                                      wordDict);
+        boolean res = lc139.wordBreak(s, wordDict);
         System.out.println("res = " + res);
     }
 }
