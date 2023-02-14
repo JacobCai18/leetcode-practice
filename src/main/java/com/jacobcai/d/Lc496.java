4183e7db5df767073dcdb3f5daaf9b03e4c3dbfe
Jacob Cai <cabbagecai18@gmail.com>
2023-02-14 15:02:04 +0800

    Lc496: Update v1.0

--- src/main/java/com/jacobcai/d/Lc496.java
+++ src/main/java/com/jacobcai/d/Lc496.java
@@ -3,34 +3,28 @@ package com.jacobcai.d;
 import java.util.*;
 
 public class Lc496 {
-    public int[] nextGreaterElement(int[] nums1,
-                                    int[] nums2) {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
         Map<Integer, Integer> map = new HashMap<>();
-        Deque<Integer> deque = new ArrayDeque<>();
-        for (int i = nums2.length - 1; i >= 0; i--) {
-            int num = nums2[i];
-            while (!deque.isEmpty() && num >= deque.getFirst()) {
-                deque.removeFirst();
-            }
-            map.put(num,
-                    deque.isEmpty() ?
-                    -1 :
-                    deque.getFirst());
-            deque.addFirst(num);
+        Deque<Integer> stk = new ArrayDeque<>();
+        for (int i = n - 1; i > -1; i--) {
+            int x = nums2[i];
+            while (!stk.isEmpty() && x >= stk.getFirst())
+                stk.removeFirst();
+            map.put(x, stk.isEmpty() ? -1 : stk.getFirst());
+            stk.addFirst(x);
         }
-        int[] res = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
+        int[] res = new int[m];
+        for (int i = 0; i < m; i++)
             res[i] = map.get(nums1[i]);
-        }
         return res;
     }
-    
+
     public static void main(String[] args) {
         int[] nums1 = {4, 1, 2};
         int[] nums2 = {1, 3, 4, 2};
         Lc496 lc496 = new Lc496();
-        int[] res = lc496.nextGreaterElement(nums1,
-                                             nums2);
+        int[] res = lc496.nextGreaterElement(nums1, nums2);
         System.out.println("res = " + Arrays.toString(res));
     }
 }
