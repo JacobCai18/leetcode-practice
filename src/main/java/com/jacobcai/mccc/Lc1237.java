cd0f870ef67d07912da14ce5cc4511e29e5a2bfd
Jacob Cai <cabbagecai18@gmail.com>
2023-02-19 21:46:23 +0800

    Lc1237-findSolution: 双指针

---
+++ src/main/java/com/jacobcai/mccc/Lc1237.java
@@ -0,0 +1,18 @@
+package com.jacobcai.mccc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lc1237 {
+//    public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
+//        var ans = new ArrayList<List<Integer>>();
+//        int x = 1, y = 1000;
+//        while (x <= 1000 && y > 0) {
+//            int res = customfunction.f(x, y);
+//            if (res < z) x++;
+//            else if (res > z) y--;
+//            else ans.add(List.of(x++, y--));
+//        }
+//        return ans;
+//    }
+}
